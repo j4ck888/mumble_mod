mumble-voip/mumble

FEAT(client): Implement tray icon highlighting
Previously, only "window highlighting" was supported
by Mumble. However, when Mumble was minimized to tray,
there was no way to observe that highlighting.

As requested in #4584, this commit introduces highlighting
to the tray icon. When a highlight message is received, the
tray icon will flash the "information icon" every two seconds
until the MainWindow of Mumble receives focus again.

To enable this for a specific message type, the user needs to
check the "highlight" box in the "Messages" configuration dialog.

Fixes #4584

src/mumble/Log.cpp
@@ -22,6 +22,8 @@
 #include "VolumeAdjustment.h"
 #include "Global.h"
 
+#include "widgets/TrayIcon.h"
+
 #include <QSignalBlocker>
 #include <QtCore/QMutexLocker>
 #include <QtGui/QImageWriter>
@@ -804,6 +806,7 @@ void Log::log(MsgType mt, const QString &console, const QString &terse, bool own
 			// Message notification with window highlight
 			if (flags & Settings::LogHighlight) {
 				QApplication::alert(Global::get().mw);
+				Global::get().trayIcon->highlight();
 			}
 
 			// Message notification with balloon tooltips

j4ck888/mumble_mod

src/mumble/Log.cpp
@@ -22,6 +22,8 @@
 #include "VolumeAdjustment.h"
 #include "Global.h"
 
+#include "widgets/TrayIcon.h"
+
 #include <QSignalBlocker>
 #include <QtCore/QMutexLocker>
 #include <QtGui/QImageWriter>
@@ -803,7 +805,7 @@ void Log::log(MsgType mt, const QString &console, const QString &terse, bool own
 		if (!(Global::get().mw->isActiveWindow() && Global::get().mw->qdwLog->isVisible())) {
 			// Message notification with window highlight
 			if (flags & Settings::LogHighlight) {
-				QApplication::alert(Global::get().mw);
+				Global::get().mw->highlightWindow();
 			}
 
 			// Message notification with balloon tooltips